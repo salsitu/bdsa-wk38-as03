bd3b8b42b6a4df10ab27a6782bc23fe0e0410e26
salsitu <sals@itu.dk>
2021-09-29 13:02:15 +0200

    Tested and implemented delegates / anonymous methods.

--- Assignment3.Tests/DelegatesTests.cs
+++ Assignment3.Tests/DelegatesTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace BDSA2021.Assignment03.Tests
@@ -6,9 +7,67 @@ namespace BDSA2021.Assignment03.Tests
     public class DelegatesTests
     {
         [Fact]
-        public void Test1()
+        public void StringFormatterReverse()
         {
+            // Arrange.
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
 
+            // Act.
+            Program.Reverse();
+
+            string expected = "esreveR";
+            string actual = writer.ToString();
+
+
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void DecimalProduct()
+        {
+            // Arrange.
+            decimal x = 2.5M;
+            decimal y = 5.5M;
+            Func<decimal, decimal, decimal> product = (x, y) => x * y;
+
+            // Act.
+            decimal expected = 13.75M;
+            decimal actual = product(x, y);
+
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsNumericallyEqualReturnsTrue()
+        {
+            // Arrange.
+            string str = " 0042";
+            int num = 42;
+
+            // Act.
+            bool expected = true;
+            bool actual = Program.IsNumericallyEqual(str, num);
+
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsNumericallyEqualReturnsFalse()
+        {
+            // Arrange.
+            string str = " 0053";
+            int num = 42;
+
+            // Act.
+            bool expected = false;
+            bool actual = Program.IsNumericallyEqual(str, num);
+
+            // Assert.
+            Assert.Equal(expected, actual);
         }
     }
 }
--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -2,11 +2,47 @@
 
 namespace BDSA2021.Assignment03
 {
-    class Program
+    public class Program
     {
+        public delegate void StringFormatter(string str);
+        public delegate bool NumChecker(string str, int num);
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static void Reverse()
+        {
+            var rev = new StringFormatter(
+                delegate (string str)
+                {
+                    for (int i = str.Length - 1; i >= 0; i--) {
+                        Console.Write(str[i]);
+                    }
+                }
+            );
+
+            rev("Reverse");
+        }
+
+        public static bool IsNumericallyEqual(string str, int num)
+        {
+            var check = new NumChecker(
+                delegate (string str, int num)
+                {
+                    bool isInt = Int32.TryParse(str, out int converted);
+
+                    if (isInt) 
+                    {
+                        return num == converted;
+                    }
+
+                    return false;
+                }
+            );
+
+            return check(str, num);
         }
     }
 }
